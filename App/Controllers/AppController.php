62a996f29b1f75df78dde97038de729357432cc9
Lucas Carregal <lucascarregal123@gmail.com>
2023-04-23 13:02:35 -0300

    Post de tweets
    foi desenvolvido a feature de post e recuperacao de tweets, de modo que esses sao salvos no banco e sao recuperados para serem apresentados na timeline

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 //os recursos do miniframework
+
+use APP\Models\Tweet;
 use MF\Controller\Action;
 use MF\Model\Container;
 
@@ -10,16 +12,42 @@ class AppController extends Action {
 
     public function timeline(){
 
+        $this->validaAutenticacao();
+
+        // recuperar os tweets do banco
+        $tweet = Container::getModel('tweet');
+
+        $tweet->__set('id_usuario', $_SESSION['id']);
+
+        $tweets = $tweet->getAll(); 
+
+        $this->view->tweets = $tweet->getAll(); 
+
+        $this->render('timeline');
+        
+    }
+
+    public function tweet(){
+
+        $this->validaAutenticacao();
+
+        $tweet = Container::getModel('Tweet');
+
+        $tweet->__set('id_usuario', $_SESSION['id']);
+        $tweet->__set('tweet', $_POST['tweet']);
+
+        $tweet->salvar();
+
+        header('location: /timeline');
+        
+    }
+
+    public function validaAutenticacao(){
         session_start();
 
-        if(!empty($_SESSION['id']) && !empty($_SESSION['nome'])){
-            $this->render('timeline');
-
-        }else{
-            header('location: /');
-
+        if(!isset($_SESSION['id']) || empty($_SESSION['id']) || !isset($_SESSION['nome']) || empty($_SESSION['nome'])){
+            header('location: /?login=erro');
         }
-        
     }
 
 }
